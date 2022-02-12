1e6c6618b3646668bfc579e911c31f81ce8c536f
Sean M <smidgley792@gmail.com>
2022-02-11 18:55:02 -0800

    Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import MainComponent from './components/MainComponent';
+import React, { useRef, useState } from 'react';
+import ModalContent from './components/ModalContent';
+import JumbotronComponent from './components/JumbotronComponent';
+import NavbarComponent from './components/NavbarComponent';
+import ReactModal from 'react-modal';
+import HomeComponent from './components/HomeComponent';
+import FooterComponent from './components/FooterComponent';
+import { Routes, Route } from 'react-router-dom';
 
+export default function MainComponent() {
+	const toggleModal = () => {
+		handleModal(!isModalOpen);
+	};
 
-function App() {
-  return (
-    <div className="App">
-      <MainComponent />
-    </div>
-  );
+	const [ isModalOpen, handleModal ] = useState(false);
+
+	return (
+		<div className="">
+			<ReactModal
+				isOpen={isModalOpen}
+				preventScroll={true}
+				shouldCloseOnOverlayClick={true}
+				onRequestClose={toggleModal}
+			>
+				<ModalContent onClick={toggleModal} />
+			</ReactModal>
+			<NavbarComponent onClick={toggleModal} />
+			<JumbotronComponent />
+			<Routes>
+				<Route path="/" element={<HomeComponent />} />
+				<Route path="/about" element={<div>ABOUT</div>} />
+			</Routes>
+			<FooterComponent />
+		</div>
+	);
 }
-
-export default App;
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { Navbar, Nav, Offcanvas, Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function NavbarComponent({ onClick }) {
 	return (
-		<Navbar sticky="top" bg="black" variant='dark' expand={false}>
-			<Container className=''>
-				<Navbar.Brand className='text-white mainFont' href="#">Jack of All Fades</Navbar.Brand>
-				<div className='m-2 w-44 flex justify-between'>
-					<Button onClick={onClick} variant="danger" >Book Now</Button>
-				<Navbar.Toggle aria-controls="offcanvasNavbar" />
+		<Navbar sticky="top" bg="black" variant="dark" expand={false}>
+			<Container className="">
+				<Navbar.Brand className="text-white mainFont" href="#">
+					Jack of All Fades
+				</Navbar.Brand>
+				<div className="m-2 w-44 flex justify-between">
+					<Button onClick={onClick} variant="danger">
+						Book Now
+					</Button>
+					<Navbar.Toggle aria-controls="offcanvasNavbar" />
 				</div>
 				<Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
-					<Offcanvas.Header closeButton className=''>
+					<Offcanvas.Header closeButton className="">
 						<Offcanvas.Title id="offcanvasNavbarLabel">Where to?</Offcanvas.Title>
 					</Offcanvas.Header>
 					<Offcanvas.Body>
 						<Nav className="justify-content-end flex-grow-1 pe-3">
-							<Nav.Link href="#action1">Home</Nav.Link>
-							<Nav.Link href="#action2">Link</Nav.Link>
+							<Link to="/">Home</Link>
+							<Nav.Link>
+								<Link to="/about">About</Link>
+							</Nav.Link>
+							<Link to="/about">About</Link>
 						</Nav>
 					</Offcanvas.Body>
 				</Navbar.Offcanvas>
